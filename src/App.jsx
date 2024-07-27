d261a2fa72f959d773c62ee73a5a923bf566f2f5
Lucas Alves <lucasasdev@gmail.com>
2024-07-27 19:04:00 -0300

    feat: add prop to inputs and add random id

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,57 @@
-import "./App.css";
 import Header from "./components/header/Header";
+import InputTask from "./components/inputTask/InputTask";
 import Tasks from "./components/tasks/Tasks";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import "./App.css";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Fist",
+      description: "First description",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: "Second",
+      description: "Second description",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      title: "Third",
+      description: "Third description",
+      isCompleted: false,
+    },
+  ]);
+
+  const handleTaskAddition = (inputTitle, inputDescription) => {
+    if (!inputTitle) {
+      alert("The title field is required.");
+      return;
+    }
+
+    const newTask = [
+      {
+        id: uuidv4(),
+        title: inputTitle,
+        description: inputDescription,
+        isCompleted: false,
+      },
+      ...tasks,
+    ];
+
+    setTasks(newTask);
+  };
+
   return (
     <>
-      <div className="container">
+      <div className="app-container">
         <Header />
-        <Tasks />
+        <InputTask handleTaskAddition={handleTaskAddition} />
+        <Tasks tasks={tasks} />
       </div>
     </>
   );
