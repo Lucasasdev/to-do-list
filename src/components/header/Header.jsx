08cb0396207b0f6eb759db3993d79010916963d1
Lucas Alves <lucasasdev@gmail.com>
2024-07-28 15:48:32 -0300

    refactor: remove unused redux dispatch

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,31 +1,8 @@
-import { addTask } from "../../redux/task/actions";
 import "./Header.css";
-import { useDispatch } from "react-redux";
 
 const Header = () => {
-  const tasksList = [
-    {
-      id: 1,
-      title: "test",
-      description: "first test",
-      isCompleted: false,
-    },
-    {
-      id: 2,
-      title: "test 2",
-      description: "second test",
-      isCompleted: false,
-    },
-  ];
-
-  const dispatch = useDispatch();
-
-  const handleTitleClick = () => {
-    dispatch(addTask(tasksList));
-  };
-
   return (
-    <div onClick={handleTitleClick}>
+    <div>
       <h1 className="header">To do Challenge</h1>
     </div>
   );
